804f30dc3b481811d40589f886ea1b15ed475f84
pedroinfante92 <166009636+pedroinfante92@users.noreply.github.com>
2025-07-07 13:25:56 +0200

    added supabase pair with updated signup component

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,29 +1,68 @@
-import { Link } from "react-router-dom"
-import { useState } from "react"
-import { UserAuth } from "../context/AuthContext"
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { UserAuth } from "../context/AuthContext";
 
 const Signup = () => {
-    const [email, setEmail] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
-    const [error, setError] = useState<string>('')
-    const [loading, setLoading] = useState<boolean>(false)
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const {session} = UserAuth()
-    console.log(session)
-    
-    return (
-        <>
-            <form className="max-w-md m-auto pt-24">
-                <h2 className="font-bold pb-2">Sign up today!</h2>
-                <p>Already have an account? <Link to="/signin">Sign in!</Link></p>
-                <div className="flex flex-col py-4">
-                    <input className="p-3 mt-6 border-2 border-blue-500" type="email" placeholder="Email" />
-                    <input className="p-3 mt-6 border-2 border-blue-500" type="password" placeholder="Password" />
-                    <button className="mt-6 p-3 w-full bg-blue-500" type="submit" disable={loading}>Signup</button>
-                </div>
-            </form>
-        </>
-    )
-}
+  const { session, signUpNewUser } = UserAuth();
+  const navigate = useNavigate();
+  console.log(session);
 
-export default Signup
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const result = await signUpNewUser(email, password);
+
+      if (result.sucess) {
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      setError("an error occurred");
+      console.error(error)
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <form className="max-w-md m-auto pt-24" onSubmit={handleSignUp}>
+        <h2 className="font-bold pb-2">Sign up today!</h2>
+        <p>
+          Already have an account? <Link to="/signin">Sign in!</Link>
+        </p>
+        <div className="flex flex-col py-4">
+          <input
+            className="p-3 mt-6 border-2 border-blue-500"
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="p-3 mt-6 border-2 border-blue-500"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+
+          />
+          <button
+            className="mt-6 p-3 w-full bg-blue-500"
+            type="submit"
+            disabled={loading}
+          >
+            Signup
+          </button>
+          {error && <p className="text-red-600 text-center pt-4">{error}</p>}
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Signup;
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,29 +1,93 @@
-import { createContext, useContext, useState, ReactNode } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  type ReactNode,
+} from "react";
+import { supabase } from "../supabaseClient";
 
 interface AuthContextType {
-    session: string | undefined;
+  session: string | undefined;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 interface AuthContextProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
-    const [session, setSession] = useState<string | undefined>(undefined);
+  const [session, setSession] = useState<string | undefined>(undefined);
 
-    return (
-        <AuthContext.Provider value={{ session }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  // Sign up
+
+  const signUpNewUser = async (email, password) => {
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      console.error("there was a problem signing up:", error);
+      return { sucess: false, error };
+    }
+    return { sucess: true, data };
+  };
+
+  // Log in
+
+  const signInUser = async ( email, password ) => {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      });
+
+      if (error) {
+        console.error("sign in error occurred: ", error);
+        return { sucess: false, error: error.message };
+      }
+
+      console.log("sign-in sucess: ", data);
+      return { sucess: true, data };
+    } catch (error) {
+      console.error("an error occurred: ", error);
+    }
+  };
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  // Sign out
+
+  const signOut = () => {
+    const { error } = supabase.auth.signOut();
+    if (error) {
+      console.log("there was an error:", error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ session, signUpNewUser, signInUser, signOut }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const UserAuth = () => {
-    const context = useContext(AuthContext);
-    if (!context) {
-        throw new Error("UserAuth must be used within an AuthContextProvider");
-    }
-    return context;
-}
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("Error");
+  }
+  return context;
+};
