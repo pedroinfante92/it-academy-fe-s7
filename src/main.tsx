39b6e2989314cc777f06d73638d0ad268e680053
pedroinfante92 <166009636+pedroinfante92@users.noreply.github.com>
2025-07-13 11:41:49 +0200

    main configuration to Providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
 import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { router } from "./router";
 import { RouterProvider } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <>
-      <h1 className="text-center text-3xl pt-4">Supabase Auth & Context</h1>
+    <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
         <RouterProvider router={router} />
       </AuthContextProvider>
-    </>
+    </QueryClientProvider>
   </StrictMode>
 );
